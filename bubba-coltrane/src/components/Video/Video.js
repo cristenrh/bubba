09dcf9fd68e86dd6c0c2eea00ecb4a0669c64163
Cristen <cristenrh0@gmail.com>
2021-03-13 16:12:17 -0600

    image fixing, more mobile adjustments, started video page

--- bubba-coltrane/src/components/Video/Video.js
+++ bubba-coltrane/src/components/Video/Video.js
@@ -1,20 +1,33 @@
+import React from 'react';
+import { withRouter } from 'react-router';
+import YouTube from 'react-youtube';
 import './Video.css';
 
-function Video() {
-    return (
-        <div className="App">
-            <div >
-                <div className="site-banner-header">
-                    <h1>
-                        Video
-                    </h1>
-                </div>
-               
-                  
-                </div>
-            </div>
-        );
-      }
+
+
+class Video extends React.Component {
+    render() {
+      const opts = {
+        height: '390',
+        width: '640',
+        playerVars: {
+          // https://developers.google.com/youtube/player_parameters
+          autoplay: 1,
+          controls:0,
+          modestbranding:1,
+          rel:0,
+          showinfo:0,
+        },
+      };
+  
+      return <YouTube videoId="GVW9piKz1o8" opts={opts} onReady={this._onReady} />;
+    }
+  
+    _onReady(event) {
+      // access to player in all event handlers via event.target
+      event.target.pauseVideo();
+    }
+  }
       
     export default Video;
       
