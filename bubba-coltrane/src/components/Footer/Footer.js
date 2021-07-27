00a1aa8cfc4ee5a6054d0c8586ced03e9df48c9c
mymaestro <mymaestro@gmail.com>
2021-07-26 22:50:17 -0500

    Fixed Instagram URL in footers

--- bubba-coltrane/src/components/Footer/Footer.js
+++ bubba-coltrane/src/components/Footer/Footer.js
@@ -28,7 +28,7 @@ function Footer() {
             </div>
             <div className="social-media">
                 <a href="//www.facebook.com/BubbaColetrane" className="icon"><i ><FontAwesomeIcon icon={faFacebookF} /></i></a>
-                <a href="//https://www.instagram.com/bubbacoltrane/" className="icon"><i ><FontAwesomeIcon icon={faInstagram} /></i></a>
+                <a href="//www.instagram.com/bubbacoltrane/" className="icon"><i ><FontAwesomeIcon icon={faInstagram} /></i></a>
                 <a href="//www.youtube.com/channel/UCbe4IPu92nn7HVOTE8jB4YA" className="icon"><i ><FontAwesomeIcon icon={faYoutube} /></i></a>
                 <a href="//music.apple.com/us/album/brew-train-ep/1514950873?uo=4&app=itunes&at=1001lry3&ct=dashboard" className="icon"><i ><FontAwesomeIcon icon={faItunes} /></i></a>
                 <a href="//open.spotify.com/album/6rHkKtc2Zl1a0sA2Dz2gTz" className="icon"><i ><FontAwesomeIcon icon={faSpotify} /></i></a>
